61609d8926b038be3d25337ebc6366cad0391437
Florian Zimmermann <flozi76@bluewin.ch>
2016-09-09 11:56:20 +0200

    routes

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -7,3 +7,28 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+// Required Dependencies
+var fs = require('fs');
+var oxford = require('project-oxford');
+var Jimp = require('jimp');
+var pngFileStream = require('png-file-stream');
+var GIFEncoder = require('gifencoder');
+
+// Handle POST request
+router.post('/', function (req, res, next) {
+    var imgSrc = req.file ? req.file.path : '';
+    Promise.resolve(imgSrc)
+        .then(function detectFace(image) {
+            console.log("TODO: detect face using Oxford API.");
+        })
+        .then(function generateBobblePermutations (response) {
+            console.log("TODO: generate multiple images with head rotated.");
+        })
+        .then(function generateGif (dimensions) {
+            console.log('TODO: generate GIF')
+            return imgSrc;
+        }).then(function displayGif(gifLocation) {
+            res.render('index', { title: 'Done!', image: gifLocation })
+        });
+});
